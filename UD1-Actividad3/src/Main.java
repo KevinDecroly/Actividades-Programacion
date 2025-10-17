37552e712a6a094d9eecbba1cd6958131cfbb713
COMPUS021\daw1 <kevinraww2589@gmail.com>
2025-10-17 10:08:13 +0200

    Actividad 3 realizada hasta ejercicio 1 y cominezo del 2

--- UD1-Actividad3/src/Main.java
+++ UD1-Actividad3/src/Main.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        /*Realiza un programa que dada una cantidad de euros que el usuario introduce por
+                /*Realiza un programa que dada una cantidad de euros que el usuario introduce por
         teclado (múltiplo de 5 €) mostrará los billetes de cada tipo que serán necesarios para
         alcanzar dicha cantidad (utilizando billetes de 500, 200, 100, 50, 20, 10 y 5). Hay que
         indicar el mínimo de billetes posible. Por ejemplo, si el usuario introduce 145 el
@@ -18,26 +16,26 @@ public class Main {
         System.out.println("Introduce los billetes que sean múltiplo de 5");
         int valor = sc.nextInt();
         if (valor    % 5 != 0) {
-            System.out.println("La cantidad debe ser múltiplo de 5.");
+            System.out.println("La cantidad no es múltiplo de 5.");
         } else {
-        int billetes[] = {5,10,20,50,100,200,500};
-        int[] usados = new int[billetes.length];
-        int resto = valor;
+            int billetes[] = {5,10,20,50,100,200,500};
+            int[] usados = new int[billetes.length];
+            int resto = valor;
 
-        for (int i = billetes.length - 1; i >= 0; i--) {
-            usados[i] = resto / billetes[i];
-            resto = resto % billetes[i];
-        }
-
-        System.out.println("Dividirlo en billetes");
-        int totalusado = 0;
-
-        for (int i = billetes.length - 1; i >= 0; i--) {
-            if (usados[i] != 0) {
-            System.out.print(usados[i] + " billetes de " + billetes[i] +  "\n");
-            totalusado += usados[i];
+            for (int i = billetes.length - 1; i >= 0; i--) {
+                usados[i] = resto / billetes[i];
+                resto = resto % billetes[i];
             }
-        }
+
+            System.out.println("Dividirlo en billetes");
+            int totalusado = 0;
+
+            for (int i = billetes.length - 1; i >= 0; i--) {
+                if (usados[i] != 0) {
+                    System.out.print(usados[i] + " billetes de " + billetes[i] +  "\n");
+                    totalusado += usados[i];
+                }
+            }
             System.out.println("Billetes usados "+ totalusado);
         }
         /*Realiza un programa que muestre un menú de opciones como el siguiente:
@@ -49,7 +47,54 @@ public class Main {
         System.out.println("Ejercicio 2");
         sc = new Scanner(System.in);
 
+        double num1 =0.0, num2 =0.0;
+        String opcion ="5";
 
+        do{
+            System.out.println("Menu de operaciones:");
+            System.out.println("1. Sumar");
+            System.out.println("2. Restar");
+            System.out.println("3. Multiplicar");
+            System.out.println("4. Dividir");
+            System.out.println("5. Salir");
+            opcion = sc.nextLine();
+
+            switch (opcion) {
+                case "1":
+                    sc = new Scanner(System.in);
+                    System.out.println("Introduce el primer numero: ");
+                    num1 = sc.nextDouble();
+                    System.out.println("Introduce el segundo numero: ");
+                    num2 = sc.nextDouble();
+                    System.out.println("La suma de los dos numeros es: "+(num1+num2));
+                    break;
+
+                case "2":
+                    sc = new Scanner(System.in);
+                    System.out.println("Introduce el primer numero: ");
+                    num1 = sc.nextDouble();
+                    System.out.println("Introduce el segundo numero: ");
+                    num2 = sc.nextDouble();
+                    System.out.println("La resta de los dos numeros es: "+(num1-num2));
+                    break;
+                case "3":
+                    sc = new Scanner(System.in);
+                    System.out.println("Introduce el primer numero: ");
+                    num1 = sc.nextDouble();
+                    System.out.println("Introduce el segundo numero: ");
+                    num2 = sc.nextDouble();
+                    System.out.println("La multiplicacion de los dos numeros es: "+(num1*num2));
+                    break;
+                case "4":
+                    sc = new Scanner(System.in);
+                    System.out.println("Introduce el primer numero: ");
+                    num1 = sc.nextDouble();
+                    System.out.println("Introduce el segundo numero: ");
+                    num2 = sc.nextDouble();
+                    System.out.println("La division de los dos numeros es: "+(num1/num2));
+                    break;
+            }
+        }while(!opcion.equals("5"));
 
 
     }
