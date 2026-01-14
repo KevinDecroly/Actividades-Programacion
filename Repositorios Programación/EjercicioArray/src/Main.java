f4d10ee619c1d92358caeeea7e5e61ab069a2853
COMPUS032\daw1 <avelascod03@educantabria.es>
2026-01-14 09:53:46 +0100

    Actividas listas terminada

--- Repositorios Programación/EjercicioArray/src/Main.java
+++ Repositorios Programación/EjercicioArray/src/Main.java
@@ -1,18 +1,131 @@
-import java.util.ArrayList;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<String> lista = new ArrayList(10);
 
+        System.out.println("Ejercicio Array");
+        ArrayList<Personas> listaPersonas = new ArrayList<Personas>(10);
+        //Añadir las 5 instancias al ArrayList
+        listaPersonas.add(new Personas("72000001X", "Kevin", "Vasquez"));
 
-        Personas p1 = new Personas("200001X","Kevin","Vasquez");
-        Personas p2 = new Personas("200001X","David","Tulcanazo");
-        Personas p3 = new Personas("200001X","Edgar","Oviedo");
-        Personas p4 = new Personas("200001X","Marco","Vasquez");
+        listaPersonas.add(new Personas("72000003X", "David", "Tulcanazo"));
 
+        listaPersonas.add(new Personas("72000005X", "Edgar", "Oviedo"));
 
+        listaPersonas.add(new Personas("72000007X", "Mario", "Pila"));
+
+        listaPersonas.add(new Personas("72000009X", "Marco", "Valiente"));
+
+        Iterator<Personas> iterator = listaPersonas.iterator();
+        while (iterator.hasNext()) {
+            Personas p = iterator.next();
+            System.out.println(p);
+        }
+
+        System.out.println("Borrar 2 elementos del array con iterator");
+        Iterator<Personas> iterator2 = listaPersonas.iterator();
+
+        while (iterator2.hasNext()) {
+            Personas persona = iterator2.next();
+
+            if (persona.getNombre().equals("Kevin") ||
+                    persona.getNombre().equals("Mario")) {
+                System.out.println("Borramos : " + persona.getNombre());
+                iterator2.remove();
+            }
+        }
+        System.out.println("Array con los 2 elementos borrados");
+        for (Personas a : listaPersonas) {
+            System.out.println(a);
+        }
+
+        System.out.println("Añadir nueva persona en mitad de la lista");
+        //Lo pondremos en la posicion 2 ya que solo tenemos la posicion 0, 1, 2 y 3 despues de haber borrado los otros 2
+        listaPersonas.add(2, new Personas("72000012X", "Nuevo", "Miembro"));
+        System.out.println("Recorremos los elementos necesarios");
+        for (int i = 0; i < listaPersonas.size(); i++) {
+            System.out.println(listaPersonas.get(i));
+        }
+        System.out.println("Mostramos la nueva lista");
+        Iterator<Personas> iterator3 = listaPersonas.iterator();
+        while (iterator3.hasNext()) {
+            Personas listapersona = iterator3.next();
+            System.out.println(listapersona);
+        }
+
+        System.out.println("Lista ordenada con compareTo");
+        listaPersonas.sort(null);
+        for (Personas p : listaPersonas) {
+            System.out.println(p);
+        }
+        System.out.println("Eliminar todos los valores del ArrayList");
+        listaPersonas.clear();
+        System.out.println(listaPersonas.size());
+
+//        System.out.println("\n");
+//        System.out.println("MISMO EJERCICIO USANDO LINKEDLIST");
+//
+//        System.out.println("Ejercicio LinkedList");
+//        LinkedList<Personas> listaPersonas = new LinkedList<Personas>();
+//        //Añadir las 5 instancias al LinkedList
+//        listaPersonas.add(new Personas("72000001X", "Kevin", "Vasquez"));
+//
+//        listaPersonas.add(new Personas("72000003X", "David", "Tulcanazo"));
+//
+//        listaPersonas.add(new Personas("72000005X", "Edgar", "Oviedo"));
+//
+//        listaPersonas.add(new Personas("72000007X", "Mario", "Pila"));
+//
+//        listaPersonas.add(new Personas("72000009X", "Marco", "Valiente"));
+//
+//        Iterator<Personas> iterator = listaPersonas.iterator();
+//        while (iterator.hasNext()) {
+//            Personas p = iterator.next();
+//            System.out.println(p);
+//        }
+//
+//        System.out.println("Borrar 2 elementos del linkedlist con iterator");
+//        Iterator<Personas> iterator2 = listaPersonas.iterator();
+//
+//        while (iterator2.hasNext()) {
+//            Personas persona = iterator2.next();
+//
+//            if (persona.getNombre().equals("Kevin") ||
+//                    persona.getNombre().equals("Mario")) {
+//                System.out.println("Borramos : " + persona.getNombre());
+//                iterator2.remove();
+//            }
+//        }
+//        System.out.println("Array con los 2 elementos borrados");
+//        for (Personas a : listaPersonas) {
+//            System.out.println(a);
+//        }
+//
+//        System.out.println("Añadir nueva persona en mitad de la lista");
+//        //Lo pondremos en la posicion 2 ya que solo tenemos la posicion 0, 1, 2 y 3 despues de haber borrado los otros 2
+//        ListIterator<Personas> it = listaPersonas.listIterator(2);
+//        it.add(new Personas("72000012X", "Nuevo", "Miembro"));
+//        System.out.println("Recorremos los elementos necesarios");
+//        for (Personas p : listaPersonas) {
+//            System.out.println(p);
+//        }
+//        System.out.println("Mostramos la nueva lista");
+//        Iterator<Personas> iterator3 = listaPersonas.iterator();
+//        while (iterator3.hasNext()) {
+//            Personas listapersona = iterator3.next();
+//            System.out.println(listapersona);
+//        }
+//
+//        System.out.println("Lista ordenada con compareTo");
+//        listaPersonas.sort(null);
+//        for (Personas p : listaPersonas) {
+//            System.out.println(p);
+//        }
+//        System.out.println("Eliminar todos los valores del LinkedList");
+//        listaPersonas.clear();
+//        System.out.println(listaPersonas.size());
     }
 }
--- Repositorios Programación/EjercicioArray/src/Personas.java
+++ Repositorios Programación/EjercicioArray/src/Personas.java
@@ -1,4 +1,6 @@
-public class Personas {
+import java.util.Objects;
+
+public class Personas implements Comparable<Personas> {
     private String dni;
     private String nombre;
     private String apellido;
@@ -8,31 +10,67 @@ public class Personas {
         this.nombre = nombre;
         this.apellido = apellido;
     }
+
     //GETTER
     public String getDni() {
+
         return dni;
     }
 
     public String getNombre() {
+
         return nombre;
     }
 
     public String getApellido() {
+
         return apellido;
     }
 
     //SETTER
     public void setDni(String dni) {
+
         this.dni = dni;
     }
 
     public void setNombre(String nombre) {
+
         this.nombre = nombre;
     }
 
     public void setApellido(String apellido) {
+
         this.apellido = apellido;
     }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Personas{");
+        sb.append("dni='").append(dni).append('\'');
+        sb.append(", nombre='").append(nombre).append('\'');
+        sb.append(", apellido='").append(apellido).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Personas personas = (Personas) o;
+        return Objects.equals(dni, personas.dni) && Objects.equals(nombre, personas.nombre) && Objects.equals(apellido, personas.apellido);
+    }
+
+    @Override
+    public int compareTo(Personas otraPersona) {
+        return this.nombre.compareToIgnoreCase(otraPersona.nombre);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dni, nombre, apellido);
+    }
+
 }
 
 
