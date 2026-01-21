8aa6c481d1b2e306608aaf19d5b971d7aa2375fd
COMPUS032\daw1 <avelascod03@educantabria.es>
2026-01-21 09:50:36 +0100

    Hacer en casa

---
+++ Repositorios Programación/Agenda/src/Contacto.java
@@ -0,0 +1,5 @@
+public class Contacto {
+    private String nombre;
+    private String telefono;
+    private String correo;
+}
---
+++ Repositorios Programación/EjemploExcepciones/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String [] nombres = new String[3];
+
+        //String s = nombres[25];
+        String n = "a";
+        int division = 23 / 0;
+
+        int num = Integer.parseInt(n);
+    }
+}
