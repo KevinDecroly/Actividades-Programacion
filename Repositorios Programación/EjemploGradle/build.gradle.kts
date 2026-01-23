dd6cd6dda727bd221ed13d6af85c4ca611515fca
COMPUS032\daw1 <avelascod03@educantabria.es>
2026-01-23 11:38:25 +0100

    sad

---
+++ Repositorios Programación/EjemploGradle/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    // Source: https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
+    implementation("com.mysql:mysql-connector-j:8.4.0")
+    // Source: https://mvnrepository.com/artifact/org.openjfx/javafx-controls
+    implementation("org.openjfx:javafx-controls:26-ea+22")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
